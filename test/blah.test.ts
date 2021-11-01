a4775e6b5bdc885326213037d5b11ee56a5e2624
GomezAgustin12 <agustingmez@gmail.com>
2021-11-01 12:09:35 -0300

    Added getObjAttrFromString test

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,22 @@
 import { sum, getObjAttrFromString } from '../src';
 
-describe('blah', () => {
+describe('sum', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+describe('[Success] Get object attribute`s value, from a string', () => {
+  it('works', () => {
+    const object = {
+      attr1: {
+        subAttr: 'value 1'
+      },
+      attr2 : 'value 2' 
+    }
+
+    const string = 'attr1.subAttr'
+
+    expect(getObjAttrFromString(object, string)).toEqual('value 1');
   });
 });
